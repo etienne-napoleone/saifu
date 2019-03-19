ee1bb95c3b8a9c5c74f671fc45f132b793e46f93
etienne-napoleone <etienne@tomochain.com>
2019-03-19 23:58:50 +0700

    docs: add doctrings for Store class

--- saifu/store.py
+++ saifu/store.py
@@ -18,19 +18,25 @@ class Store:
         self.key = tinydb.Query()
 
     def insert(self, data):
+        """Insert an item in the store"""
         return self.db.insert(data)
 
     def update(self, data, query):
+        """Update an item in the store"""
         return self.db.update(data, query)
 
     def remove(self, query):
+        """Remove an item in the store"""
         return self.db.remove(query)
 
     def search(self, query):
+        """Search items in the store"""
         return self.db.search(query)
 
     def all(self):
+        """Get all items from store"""
         return self.db.all()
 
     def purge(self):
+        """Empty the store"""
         return self.db.purge()
