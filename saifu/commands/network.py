f32a5e4389f123d9994868a99aec0a7d0c0fc4d3
etienne-napoleone <etienne@tomochain.com>
2019-04-18 15:42:19 +0700

    feat: add help text to networks subommands

--- saifu/commands/network.py
+++ saifu/commands/network.py
@@ -6,11 +6,13 @@ from saifu import views
 
 @click.group()
 def network():
+    """Manage the different networks saifu can connect to"""
     pass
 
 
 @network.command()
 def list():
+    """List all available networks"""
     n = NetworksManager()
     views.network.list(n.list())
 
@@ -21,6 +23,7 @@ def list():
 @click.option('--chain_id', prompt=f'{views.QUESTION_BULLET} Network chain id')
 @click.option('--ticker', prompt=f'{views.QUESTION_BULLET} Network ticker')
 def add(name, rpc_url, chain_id, ticker):
+    """Add a network"""
     n = NetworksManager()
     n.new(name, rpc_url, chain_id, ticker)
     views.network.add(name, rpc_url, chain_id, ticker)
@@ -29,6 +32,7 @@ def add(name, rpc_url, chain_id, ticker):
 @network.command()
 @click.option('--name', prompt=f'{views.QUESTION_BULLET} Network name')
 def rm(name):
+    """Remove a network"""
     n = NetworksManager()
     n.rm(name)
     views.network.rm(name)
@@ -37,6 +41,7 @@ def rm(name):
 @network.command()
 @click.option('--name', prompt=f'{views.QUESTION_BULLET} Network name')
 def select(name):
+    """Select which network to connect to"""
     n = NetworksManager()
     n.select(name)
     views.network.select(name)
--- saifu/main.py
+++ saifu/main.py
@@ -4,10 +4,10 @@ from saifu import __version__
 from saifu.commands.network import network
 
 
-@click.group(help=('Saifu (財布) is a cli wallet for TomoChain'))
+@click.group()
 @click.version_option(version=__version__)
 def entrypoint():
-    """Saifu cli entrypoint"""
+    """Saifu is a cli wallet for TomoChain and Ethereum based chains"""
     pass
 
 
