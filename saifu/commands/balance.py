38a32bb7c5c91a50ffba6368f8fa387241ee24d2
etienne-napoleone <etienne@tomochain.com>
2019-05-06 01:25:50 +0700

    feat: add draft for balance command

---
+++ saifu/commands/balance.py
@@ -0,0 +1,16 @@
+import click
+
+from saifu.accounts import AccountsManager
+from saifu.networks import NetworksManager
+# from saifu import views
+
+a = AccountsManager()
+n = NetworksManager()
+
+
+@click.command()
+def balance():
+    """Display the balance"""
+    address = a.inspect(a.selected())['address']
+    web3 = n.get(n.selected())
+    click.echo(web3.eth.getBalance(address))
