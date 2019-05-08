5f89a1ae5b9a063448b8bc81a2fa90ed513377b6
etienne-napoleone <etienne@tomochain.com>
2019-05-08 23:21:29 +0700

    feat: add balance view

--- saifu/commands/balance.py
+++ saifu/commands/balance.py
@@ -2,7 +2,7 @@ import click
 
 from saifu.accounts import AccountsManager
 from saifu.networks import NetworksManager
-# from saifu import views
+from saifu import views
 
 a = AccountsManager()
 n = NetworksManager()
@@ -13,6 +13,6 @@ def balance():
     """Display the balance"""
     address = a.inspect(a.selected())['address']
     web3 = n.get(n.selected())
-    click.echo(
-        web3.fromWei(web3.eth.getBalance(address), 'ether')
-    )
+    balance = web3.fromWei(web3.eth.getBalance(address), 'ether')
+    ticker = n.inspect(n.selected())['ticker']
+    views.balance.balance(balance, ticker)
--- saifu/views/__init__.py
+++ saifu/views/__init__.py
@@ -1,10 +1,11 @@
 import click
 
 from saifu.views import account
-from saifu.views import network
+from saifu.views import balance
 from saifu.views import message
+from saifu.views import network
 
-__all__ = ['account', 'network', 'message']
+__all__ = ['account', 'balance', 'message', 'network']
 
 QUESTION_BULLET = f'{click.style("?", fg="cyan")}'
 SELECTED_INDICATOR = '•'
